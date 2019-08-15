e699a4c6582720de57529dd6e5d536ce0823b7b0
Anthony Webb <anthonywebb510@gmail.com>
2019-08-15 00:58:03 -0400

    Add files via upload

--- Dice_Rolling_Sim_V01.py
+++ Dice_Rolling_Sim_V01.py
@@ -6,5 +6,33 @@ print ('With this program, you will have the option to roll a 4, 6, 8, 10, 12 or
 
 while True:
 	running=True
-		action1=input('What action would you like to take?  ')
-		if action1=
+		rType=input ('What action would you like to take?  ')
+		if rType=='4' or rType=='d4' or rType=='D4' or rType=='Dice 4' or rtype=='4 Side' or rType=='4 side' or rType=='4 Sided' or rType=='4 sided'
+		
+		running=False
+
+		if rType=='6' or rType=='d6' or rType=='D6' or rType=='Dice 6' or rtype=='6 Side' or rType=='6 side' or rType=='6 Sided' or rType=='6 sided'
+		
+		running=False
+
+		if rType=='8' or rType=='d8' or rType=='D8' or rType=='Dice 8' or rType=='8 Side' or rType=='8 side' or rType=='8 Sided' or rType=='8 sided'
+		
+		running=False
+
+		if rType=='10' or rType=='d10' or rType=='D10' or rType=='Dice 10' or rType=='10 Side' or rType=='10 side' or rType=='10 Sided' or rType=='10 sided'
+		
+		running=False
+
+		if rType=='12' or rType=='d12' or rType=='D12' or rType=='Dice 12' or rType=='12 Side' or rType=='12 side' or rType=='12 Sided' or rType=='12 sided'
+		
+		running=False
+
+		if rType=='20' or rType=='d20' or rType=='D20' or rType=='Dice 20' or rType=='20 Side' or rType=='20 side' or rType=='20 Sided' or rType=='20 sided'
+		
+		running=False
+
+	action1=input ('Would you like to roll again? (yes/no) ')
+	if action1=='no' or action1=='n':
+		break
+
+print ('Thank you!')
