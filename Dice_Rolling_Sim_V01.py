7d514bfd6b6638a35022b4e778d05dfe04f7d9da
Anthony Webb <anthonywebb510@gmail.com>
2019-08-15 00:55:09 -0400

    Add files via upload

---
+++ Dice_Rolling_Sim_V01.py
@@ -0,0 +1,10 @@
+#Program #4 - Dice Rolling Simulator
+#Dice_Rolling_Simulator_V0.1
+
+print ('Welcome to Dice Rolling Sim v0.1 by a.webb')
+print ('With this program, you will have the option to roll a 4, 6, 8, 10, 12 or 20 sided dice. The choice is yours...')
+
+while True:
+	running=True
+		action1=input('What action would you like to take?  ')
+		if action1=
